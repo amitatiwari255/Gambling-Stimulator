9c23ae5ec5e370644883b9f71ccf6dbbb8dabdfa
amitatiwari255 <amita.tiwari255@gmail.com>
2023-02-04 11:49:26 +0530

    Added:Gambler make a bet of so either win or lose

---
+++ src/com/bridgelabz/gamblinggame/GamblingStimulator.java
@@ -0,0 +1,18 @@
+package com.bridgelabz.gamblinggame;
+public class GamblingStimulator {
+    public static void main(String[] args) {
+        int stake = 100;
+        int bet = 1 ;
+        System.out.println("Welcome to gambling simulator");
+        double Betting = Math.floor(Math.random()*100)%2;
+        if (Betting ==bet) {
+            stake++;
+            System.out.println("Gambler is winning $1");
+            System.out.println("stake" +stake);
+        }
+        else {
+            stake--;
+            System.out.println("Gambler is loosing");
+        }
+    }
+}
